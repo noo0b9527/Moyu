853637c5dc1321fdf37e7c98fb46b09de32c2da4
noo0b9527 <1337306076@qq.com>
2025-06-11 23:48:04 +0800

    add Assert Module

--- Engine/Source/Runtime/Runtime/Main.cpp
+++ Engine/Source/Runtime/Runtime/Main.cpp
@@ -5,6 +5,7 @@
 
 int main()
 {
+    MOYU_ASSERT(false == false);
     Moyu::Log().Info("这是一个测试信息{}", 12121212);
     std::cout << "Hello";
     return 0;
--- Engine/Source/Runtime/Utilities/Include/Utilities/Assert.h
+++ Engine/Source/Runtime/Utilities/Include/Utilities/Assert.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include "Utilities/Log.h"
+
+#if !defined(MOYU_RELEASE)
+#define MOYU_DEVELOPMENT_TOOLS 1
+#endif
+
+#if defined(_WIN32) && defined(MOYU_DEVELOPMENT_TOOLS)
+#define MOYU_DEBUG_BREAK() __debugbreak()
+#endif
+
+#if defined(MOYU_DEVELOPMENT_TOOLS)
+#define MOYU_ASSERT(cond) \
+    do \
+    { \
+        if (!(cond)) \
+        { \
+            Moyu::Logger logger = Moyu::Log(); \
+            logger.Fatal(#cond); \
+            MOYU_DEBUG_BREAK(); \
+        } \
+    } while (0)
+
+#else
+#define MOYU_ASSERT(cond)
+#endif
